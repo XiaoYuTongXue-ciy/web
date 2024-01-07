5ebd2134eecaf74ee46d3dbfa4bb18156c9abe75
andrew <1144098817@qq.com>
2024-01-07 17:03:53 +0800

    fix(view): 教师管理 bug fix

--- src/views/teacher/detailDrawer.vue
+++ src/views/teacher/detailDrawer.vue
@@ -7,7 +7,7 @@
     width="500px"
     @ok="handleSubmit"
   >
-    <BasicForm @register="registerForm" />
+    <div ref="wrapEl"> <BasicForm @register="registerForm" /></div>
   </BasicDrawer>
 </template>
 <script lang="ts" setup>
@@ -15,16 +15,26 @@
   import { BasicForm, useForm } from '@/components/Form/index';
   import { addFormSchema } from './config';
   import { BasicDrawer, useDrawerInner } from '@/components/Drawer';
-  import { add, update } from '@/api/teacher';
+  import { useLoading } from '@/components/Loading';
+  import { add, update, getDetailInfo } from './api';
 
   const emit = defineEmits(['success', 'register']);
   const isUpdate = ref(true);
+  const wrapEl = ref();
 
   const [registerForm, { resetFields, setFieldsValue, validate }] = useForm({
     labelWidth: 90,
     baseColProps: { span: 24 },
     schemas: addFormSchema,
     showActionButtonGroup: false,
+  });
+
+  const [openWrapLoading, closeWrapLoading] = useLoading({
+    target: wrapEl,
+    props: {
+      tip: '加载中...',
+      absolute: true,
+    },
   });
 
   const [registerDrawer, { setDrawerProps, closeDrawer }] = useDrawerInner(async (data) => {
@@ -37,9 +47,12 @@
     isUpdate.value = !!data?.isUpdate;
 
     if (unref(isUpdate)) {
-      console.log('data', data.record);
+      openWrapLoading();
+      const detail = await getDetailInfo({ id: data.record?.id }).catch(() => closeWrapLoading());
+      closeWrapLoading();
+
       setFieldsValue({
-        ...data.record,
+        ...detail,
       });
     }
   });
---
+++ src/views/teacher/utils/index.js
@@ -0,0 +1,26 @@
+// 性别
+export const genderOptions = [
+  {
+    label: '未说明性别',
+    value: 0,
+  },
+  {
+    label: '男',
+    value: 1,
+  },
+  {
+    label: '女',
+    value: 2,
+  },
+  {
+    label: '其他',
+    value: 3,
+  },
+];
+
+export const genderMap = {
+  0: '未说明性别',
+  1: '男',
+  2: '女',
+  3: '其他',
+};
